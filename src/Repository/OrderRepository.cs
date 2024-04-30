796f3dfe77f22339fa2a6f29568b6741be64fdd6
OmniaAliZ <zzomniaa@gmail.com>
2024-04-30 15:17:20 +0300

    Order CRUD + address data

---
+++ src/Repository/OrderRepository.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using sda_onsite_2_csharp_backend_teamwork.src.Abstraction;
+using sda_onsite_2_csharp_backend_teamwork.src.Database;
+using sda_onsite_2_csharp_backend_teamwork.src.Entity;
+
+namespace sda_onsite_2_csharp_backend_teamwork.src.Repository;
+
+public class OrderRepository : IOrderRepository
+{
+    public IEnumerable<Order> Orders;
+    public OrderRepository()
+    {
+
+        Orders = new DatabaseContext().Orders;
+    }
+    public IEnumerable<Order> FindAll()
+    {
+        return Orders;
+    }
+    public Order? FindOne(string id)
+    {
+        Order? order = Orders.FirstOrDefault(order => order.Id == id);
+        if (order is not null)
+        {
+            return order;
+        }
+        else return null;
+    }
+    public Order CreateOne(Order order)
+    {
+        Orders.Append(order);
+        return order;
+    }
+    public Order UpdateOne(Order updatedOrder)
+    {
+        var orders = Orders.Select(order =>
+         {
+             if (order.Id == updatedOrder.Id)
+             {
+                 return updatedOrder;
+             }
+             return order;
+         });
+        Orders = orders.ToList();
+
+        return updatedOrder;
+    }
+    public IEnumerable<Order>? DeleteOne(string id)
+    {
+        Order? order = FindOne(id);
+        if (order is not null)
+        {
+            var orders = Orders.Where(order => order.Id != id);
+            Orders = orders;
+            return Orders;
+        }
+        return null;
+    }
+
+}
