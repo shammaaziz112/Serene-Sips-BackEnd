d54740fe8701258cde999ded63ae2066e00e251e
Raghadfah <158806995+Raghadfah@users.noreply.github.com>
2024-04-30 17:11:56 +0300

    Still Fixing

--- src/Repository/AddressRepository.cs
+++ src/Repository/AddressRepository.cs
@@ -2,9 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using sda_onsite_2_csharp_backend_teamwork.src.Abstraction;
+using sda_onsite_2_csharp_backend_teamwork.src.Entity;
 
 namespace sda_onsite_2_csharp_backend_teamwork.src.Repository;
-    public class AddressRepository
+    public class AddressRepository : IAddressRepository
     {
-        
+         public IEnumerable<Address> Addressess;
+    public AddressRepository()
+    {
+
+        Addressess = new DatabaseContext().Addressess;
+    }
+    public IEnumerable<Address> FindAll()
+    {
+        return Addressess;
+    }
+    public Address? FindOne(string id)
+    {
+        Address? address = Addressess.FirstOrDefault(order => order.Id == id);
+    
+
+    }
     }
