70a6a6a3140c49628cd8023b34323a69df44edcc
OmniaAliZ <zzomniaa@gmail.com>
2024-05-01 16:30:57 +0300

    delete updated

--- src/Abstraction/IOrderService.cs
+++ src/Abstraction/IOrderService.cs
@@ -12,6 +12,6 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.Abstraction
         public Order? FindOne(string orderId);
         public Order CreateOne(Order order);
         public Order? UpdateOne(string email, Order order);
-        public IEnumerable<Order>? DeleteOne(string id);
+        public bool DeleteOne(string id);
     }
 }
