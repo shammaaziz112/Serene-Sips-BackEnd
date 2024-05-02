f75f059bc22a8839dae6589cb41e1f7c15c23804
sara-khaled1 <saraaljaber501@gmail.com>
2024-05-02 12:02:58 +0300

    Category & OrderItem

--- src/Abstraction/IOrderItemRepository.cs
+++ src/Abstraction/IOrderItemRepository.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using sda_onsite_2_csharp_backend_teamwork.src.Entity;
-
-namespace sda_onsite_2_csharp_backend_teamwork.src.Abstraction
+namespace sda_onsite_2_csharp_backend_teamwork.src.Abstraction;
+public interface IOrderItemRepository
 {
-    public interface IOrderItemRepository
-    {
-        public List<OrderItem> FindAll(OrderItem orderitem);
-        public OrderItem? FindOne(string id);
-        public OrderItem CreateOne(OrderItem orderitem);
-        public OrderItem UpdateOne(OrderItem orderitem);
-        public OrderItem DeleteOne(string id);
+    public List<OrderItem> FindAll();
+    public OrderItem? FindOne(string id);
+    public OrderItem CreateOne(OrderItem orderitem);
+    public OrderItem UpdateOne(OrderItem orderitem);
+    public bool DeleteOne(string id);
 
-    }
 }
--- src/Abstraction/IOrderItemService.cs
+++ src/Abstraction/IOrderItemService.cs
@@ -1,24 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using sda_onsite_2_csharp_backend_teamwork.src.Entity;
 using sda_onsite_2_csharp_backend_teamwork.src.DTO;
+namespace sda_onsite_2_csharp_backend_teamwork.src.Abstraction;
 
-using AutoMapper;
-
-
-namespace sda_onsite_2_csharp_backend_teamwork.src.Abstraction
-{
     public interface IOrderItemService
     {
         public IEnumerable<OrderItemReadDto> FindAll();
         public OrderItemReadDto? FindOne(string id);
         public OrderItemReadDto CreateOne(OrderItem orderitem);
         public OrderItemReadDto? UpdateOne(string id, OrderItem orderitem);
-            public bool DeleteOne(string id);
+        public bool DeleteOne(string id);
 
 
     }
-}
